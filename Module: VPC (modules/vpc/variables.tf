3480d890317d38b81ab09903f8f3da6cf2151926
Nimra589 <146828759+Nimra589@users.noreply.github.com>
2025-08-02 19:24:51 +0500

    Delete Module: VPC (modules/vpc directory

--- Module: VPC (modules/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "aws_region" {
-  description = "AWS region for subnets"
-  type        = string
-}
