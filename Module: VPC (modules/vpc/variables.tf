ea11a0e24949e8c7c607781132ee88efe24b60ed
Nimra589 <146828759+Nimra589@users.noreply.github.com>
2025-08-02 18:21:22 +0500

    Create variables.tf

---
+++ Module: VPC (modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region for subnets"
+  type        = string
+}
